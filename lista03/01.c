8f3e29f64cb04362dfbae9a6078ab8630a2a4378
mesquita97 <mesquitagabriel97@gmail.com>
2017-11-06 21:08:30 -0200

    adicionado exercicio 01

---
+++ lista03/01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int conta_vogal(char entrada [])
+{
+	int cont = 0;
+	for(int i=0; i<strlen(entrada); i++)
+	{
+		if ((entrada[i] == 'a')||(entrada[i] == 'e')||(entrada[i] =='i')||(entrada[i] == 'o')||(entrada[i] == 'u')) cont++;
+	}
+	return cont;
+}
+	
+
+int main()
+{	
+	char entrada[255]; 
+	fgets(entrada, 255, stdin);
+
+	printf("%d\n", conta_vogal(entrada));
+	return 0;
+}
